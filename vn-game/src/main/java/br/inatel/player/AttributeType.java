b051c99bbdd441c6e310309c573e1fbe9b9d237c
zSh3rl0cK <pedrohrdias12@gmail.com>
2026-01-23 19:38:23 -0300

    feat: add new  rpgattributes logic

---
+++ vn-game/src/main/java/br/inatel/player/AttributeType.java
@@ -0,0 +1,9 @@
+package br.inatel.player;
+
+public enum AttributeType {
+    STR,   // Strength
+    DEX,   // Dexterity
+    INT,   // Intelligence
+    PRES,  // Presence
+    VIG    // Vigor
+}
---
+++ vn-game/src/main/java/br/inatel/player/Attributes.java
@@ -0,0 +1,40 @@
+package br.inatel.player;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class Attributes {
+
+    private Map<AttributeType, Integer> attributes;
+    private int availablePoints;
+
+    public Attributes(int initialPoints) {
+        this.availablePoints = initialPoints;
+        attributes = new EnumMap<>(AttributeType.class);
+
+        for (AttributeType type : AttributeType.values()) {
+            attributes.put(type, 0);
+        }
+    }
+
+    public void addPoint(AttributeType type) {
+        if (availablePoints <= 0) {
+            throw new IllegalStateException("Não há pontos disponíveis.");
+        }
+
+        attributes.put(type, attributes.get(type) + 1);
+        availablePoints--;
+    }
+
+    public int get(AttributeType type) {
+        return attributes.get(type);
+    }
+
+    public void gainPoint() {
+        availablePoints++;
+    }
+
+    public int getAvailablePoints() {
+        return availablePoints;
+    }
+}
