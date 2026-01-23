4636deadf3ed0bc89d3e0dbe8efe62b100a5b7e7
zSh3rl0cK <pedrohrdias12@gmail.com>
2026-01-23 20:50:36 -0300

    feat: add base logic to swords

--- vn-game/src/main/java/br/inatel/Items/Weapons/Swords/BaseSword.java
+++ vn-game/src/main/java/br/inatel/Items/Weapons/Swords/BaseSword.java
@@ -1,4 +1,8 @@
 package br.inatel.Items.Weapons.Swords;
 
-public class BaseSword {
+public class BaseSword extends Swords {
+
+    public BaseSword() {
+        this.baseDamage = 10;
+    }
 }
--- vn-game/src/main/java/br/inatel/Items/Weapons/Swords/Swords.java
+++ vn-game/src/main/java/br/inatel/Items/Weapons/Swords/Swords.java
@@ -1,4 +1,12 @@
 package br.inatel.Items.Weapons.Swords;
 
-public class Swords {
+import br.inatel.Items.Items;
+
+public abstract class Swords extends Items {
+
+    protected float baseDamage;
+
+    public float calculateDamage(float strength, float vigor) {
+        return baseDamage + (strength * 2) + (vigor * 0.5f);
+    }
 }
