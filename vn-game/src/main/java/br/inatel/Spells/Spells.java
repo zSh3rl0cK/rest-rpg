b9ed0ffaad54e999b36838f65d3559b5d9ba401d
zSh3rl0cK <pedrohrdias12@gmail.com>
2026-01-23 19:32:42 -0300

    feat: add base attributes and casting method

--- vn-game/src/main/java/br/inatel/Spells/Spells.java
+++ vn-game/src/main/java/br/inatel/Spells/Spells.java
@@ -1,21 +1,33 @@
 package br.inatel.Spells;
 
-public class Spells {
-    protected String name;
+public abstract class Spells {
 
-    public Spells(String name) {
+    protected String name;
+    protected int mpCost;
+    protected float basePower;
+
+    public Spells(String name, int mpCost, float basePower) {
         this.name = name;
+        this.mpCost = mpCost;
+        this.basePower = basePower;
     }
 
-    public void cast() {
+    public float calculateFinalPower(float staffMultiplier) {
+        return basePower * staffMultiplier;
+    }
 
+    public void cast(float finalPower) {
+        // Por enquanto apenas representação do efeito
+        System.out.println(
+                name + " foi conjurado com poder " + finalPower
+        );
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getMpCost() {
+        return mpCost;
     }
 }
