4ce16499b3397eb6685eb680fd9e039cbd8fb462
zSh3rl0cK <pedrohrdias12@gmail.com>
2026-01-24 20:41:20 -0300

    refactor: refactor route response

--- vn-game/src/main/java/br/inatel/controllers/DiceController.java
+++ vn-game/src/main/java/br/inatel/controllers/DiceController.java
@@ -1,6 +1,7 @@
 package br.inatel.controllers;
 
 import br.inatel.models.dice.Dices;
+import br.inatel.service.Dice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,7 @@ public class DiceController {
     private Dices dice;
 
     @GetMapping("/roll")
-    public String rollDice(@RequestParam(defaultValue = "none") int faces) {
-        return "Dado puro: " + dice.rollSingleDice(faces);
+    public Dice rollDice(@RequestParam(defaultValue = "none") int faces) {
+        return new Dice(faces, dice.rollSingleDice(faces));
     }
 }
