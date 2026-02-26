3d8633596d10a38207f13d3cb9b0388b7f88f73b
zSh3rl0cK <pedrohrdias12@gmail.com>
2026-02-25 22:15:02 -0300

    feat: add new offensive spell interface

---
+++ vn-game/src/main/java/br/inatel/domain/Spells/utils/OffensiveSpell.java
@@ -0,0 +1,7 @@
+package br.inatel.domain.Spells.utils;
+
+import br.inatel.domain.player.LivingBeing;
+
+public interface OffensiveSpell {
+    public void castOffensiveSpell(LivingBeing target);
+}
