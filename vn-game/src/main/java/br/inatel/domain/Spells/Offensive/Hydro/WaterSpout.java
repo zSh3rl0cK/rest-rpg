f21fab3c61e8ef85a4c420ebd3290442dfe8cd30
zSh3rl0cK <pedrohrdias12@gmail.com>
2026-02-25 22:16:07 -0300

    feat: add casting methods

--- vn-game/src/main/java/br/inatel/domain/Spells/Offensive/Hydro/WaterSpout.java
+++ vn-game/src/main/java/br/inatel/domain/Spells/Offensive/Hydro/WaterSpout.java
@@ -1,12 +1,28 @@
 package br.inatel.domain.Spells.Offensive.Hydro;
 
+import br.inatel.domain.Spells.utils.OffensiveSpell;
 import br.inatel.domain.Spells.utils.SpellType;
 import br.inatel.domain.Spells.utils.Spells;
+import br.inatel.domain.player.LivingBeing;
+import br.inatel.exception.SpellException;
 
-public class WaterSpout extends Spells {
-
-
+public class WaterSpout extends Spells implements OffensiveSpell {
     public WaterSpout(String name) {
         super(name, 6, SpellType.DAMAGE, 10);
+    }
+
+
+    @Override
+    public void castSpell(Spells spell) {
+
+    }
+
+    @Override
+    public void castOffensiveSpell(LivingBeing target) {
+        try{
+            target.setLife(target.getLife() - this.basePower);
+        }catch (Exception e){
+            throw new SpellException("Error casting offensive spell");
+        }
     }
 }
--- vn-game/src/main/java/br/inatel/domain/Spells/Offensive/Pyro/PyroBlast.java
+++ vn-game/src/main/java/br/inatel/domain/Spells/Offensive/Pyro/PyroBlast.java
@@ -1,10 +1,29 @@
 package br.inatel.domain.Spells.Offensive.Pyro;
 
+import br.inatel.domain.Spells.utils.OffensiveSpell;
 import br.inatel.domain.Spells.utils.SpellType;
 import br.inatel.domain.Spells.utils.Spells;
+import br.inatel.domain.player.LivingBeing;
+import br.inatel.exception.SpellException;
 
-public class PyroBlast extends Spells {
+public class PyroBlast extends Spells implements OffensiveSpell {
     public PyroBlast(String name) {
         super(name, 6, SpellType.DAMAGE, 10);
+    }
+
+    //todo: implementar as características específicas
+
+    @Override
+    public void castSpell(Spells spell) {
+
+    }
+
+    @Override
+    public void castOffensiveSpell(LivingBeing target) {
+        try{
+            target.setLife(target.getLife() - this.basePower);
+        }catch (Exception e){
+            throw new SpellException("Error casting offensive spell");
+        }
     }
 }
